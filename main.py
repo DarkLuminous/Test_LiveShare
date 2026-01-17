b669cd03a604c7f3bc9b36982f52d125eaf3d1f0
DarkLuminous <roger.bao101@gmail.com>
2026-01-17 08:58:02 +0800

    Add basic arithmetic functions for addition and subtraction

---
+++ main.py
@@ -0,0 +1,15 @@
+def add(x, y):
+    return x + y
+
+x = int(input("enter num1: "))
+y = int(input("enter num2: "))
+
+print(x, "+", y, "=", add(x, y))
+
+def sub(x,y):
+    return x - y
+
+x = int(input("enter num1: "))
+y = int(input("enter num2: "))
+
+print(x, "-", y, "=", sub(x, y))
